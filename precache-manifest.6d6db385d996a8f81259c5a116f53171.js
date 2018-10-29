dd3e888fcd12ba97a797a5cb1ad09ed1567153ec
Marcel Rieger <rieger@i4mc.local>
2018-10-29 15:03:37 +0100

    init

---
+++ precache-manifest.6d6db385d996a8f81259c5a116f53171.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee144b716f5fb64487c8476730953f37",
+    "url": "/static/media/talk-bubbla.ee144b71.png"
+  },
+  {
+    "revision": "c00d2e6d4081cd0587baa9b2a9be24c8",
+    "url": "/static/media/soekarno-orasi.c00d2e6d.png"
+  },
+  {
+    "revision": "6f73b7d9035e94fd16fe4afa360342ec",
+    "url": "/static/media/date-17april.6f73b7d9.png"
+  },
+  {
+    "revision": "1142e02a1ea4352ee3f8fdfa858aa6b2",
+    "url": "/static/media/bangers-regular.1142e02a.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65bd6daa03537a6f87d2",
+    "url": "/static/js/main.65bd6daa.chunk.js"
+  },
+  {
+    "revision": "87db8fe15d169c407cb4",
+    "url": "/static/js/1.87db8fe1.chunk.js"
+  },
+  {
+    "revision": "65bd6daa03537a6f87d2",
+    "url": "/static/css/main.3e83666f.chunk.css"
+  },
+  {
+    "revision": "7b6dd58f5c31ff4f9f98f8ef4958ca04",
+    "url": "/index.html"
+  }
+];
